0d9040b592776344acad4a67b8fcde1f2db91658
Ayasha Hossain Jui <jui1801@cseku.ac.bd>
2024-09-30 16:39:46 +0600

    sec 3 video 70

---
+++ cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+//	command line runner
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+
+		return args -> {
+			System.out.println("Hello World");
+		};
+	}
+
+}
