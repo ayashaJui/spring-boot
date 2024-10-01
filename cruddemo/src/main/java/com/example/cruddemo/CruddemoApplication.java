524c01c2ce84325ab29ca1861ae4e97b793e6c32
Ayasha Hossain Jui <jui1801@cseku.ac.bd>
2024-10-01 16:48:09 +0600

    video no 77

--- cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.cruddemo;
 
+import com.example.cruddemo.dao.StudentDAO;
+import com.example.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,11 +16,68 @@ public class CruddemoApplication {
 
 //	command line runner
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return args -> {
-			System.out.println("Hello World");
+//			createStudent(studentDAO);
+
+			//create multiple student
+//			createMultipleStudent(studentDAO);
+
+			//find by id
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		// create student object
+		System.out.println("Creating new student...");
+		Student tempStudent = new Student("Jane", "Doe", "jane@gmail.com");
+
+		// save the student
+		System.out.println("Saving the student...");
+//		studentDAO.save(tempStudent);
+
+		// display id of student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+
+		// retrieve student based on id
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		// display student
+		System.out.println("Found student: " + myStudent);
+	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student.....");
+		Student tempStudent1 = new Student("Jane", "Doe", "jane@gmail.com");
+		Student tempStudent2 = new Student("James", "Doe", "james@gmail.com");
+		Student tempStudent3 = new Student("Paul", "Doe", "paul@gmail.com");
+
+		// save the object
+		System.out.println("Saving the student.....");
+
+//		studentDAO.save(tempStudent1);
+//		studentDAO.save(tempStudent2);
+//		studentDAO.save(tempStudent3);
+
+
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student.....");
+		Student tempStudent = new Student("John", "Doe", "john@gmail.com");
+
+		// save the object
+		System.out.println("Saving the student.....");
+		studentDAO.save(tempStudent);
+
+		// display id of save student
+		System.out.println("Saved Student. Generated ID: "+ tempStudent.getId());
 	}
 
 }
---
+++ cruddemo/src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,30 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+}
